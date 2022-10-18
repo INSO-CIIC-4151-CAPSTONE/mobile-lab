d7860dc9c1026dcb811a92d7262f4c5979abcb28
Francis Patron <francis.patron1@upr.edu>
2022-10-17 23:41:02 -0300

    heroku settup

--- mobilab/settings.py
+++ mobilab/settings.py
@@ -137,7 +137,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'staticfiles')
+    os.path.join(BASE_DIR, 'static')
 ]
 
 # Default primary key field type
